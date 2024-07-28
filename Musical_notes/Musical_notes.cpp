58d86d0ef59ad40d4b9d9662967e5bf3980eb588
Andrei_Gridin <andreigridin918@gmail.com>
2024-07-28 20:13:26 +0500

    Добавьте файлы проекта.

---
+++ Musical_notes/Musical_notes.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+enum Note
+{
+    DO = 1,
+    RE = 2,
+    MI = 3,
+    FA = 4,
+    SOL = 5,
+    LA = 6,
+    SI = 7
+};
+
+int main()
+{
+    system("color 70");
+    system("chcp 1251>nul");
+
+    string notes;
+    cout << " Введите номера нот: ";
+    cin >> notes;
+
+    system("pause>nul");
+    return 0;
+}
